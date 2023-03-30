09c8e8f317096d703569c3e57e14bbf4427a30a3
tigmanshu-joshi <tigmanshujoshi21@gmail.com>
2023-03-30 23:29:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,140 @@
+import React, { useRef, useState } from 'react';
+import './App2.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+
+firebase.initializeApp({
+  
+  apiKey: "AIzaSyAG4ugpL9qUYLnNfB1UgGE2tI4MjXH3xBg",
+  authDomain: "gcpro-cecdd.firebaseapp.com",
+  projectId: "gcpro-cecdd",
+  storageBucket: "gcpro-cecdd.appspot.com",
+  messagingSenderId: "860382421673",
+  appId: "1:860382421673:web:c6bba1510b522ce1067fb8"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
+
+
+function App() {
+
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Xpress-Chatroom</h1>
+        <SignOut />
+      </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+
+    </div>
+  );
+}
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+
+
+      <h1>Welcome to Xpress-Chatroom !</h1>
+      <button className="sign-in" class = "btn" onClick={signInWithGoogle}>Sign in with your Google Account</button>
+      <p class = "txt">Please do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" class= "btn-2" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+const CryptoJS = require('crypto-js');
+
+const encrypt = (text) => {
+  return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(text));
+};
+
+const decrypt = (data) => {
+  return CryptoJS.enc.Base64.parse(data).toString(CryptoJS.enc.Utf8);
+};
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Write a message" />
+
+      <button type="submit" disabled={!formValue}><i class="fa-sharp fa-solid fa-paper-plane"></i></button>
+
+    </form>
+  </>)
+}
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <p>{text}</p>
+    </div>
+  </>)
+}
+
+
+export default App;
